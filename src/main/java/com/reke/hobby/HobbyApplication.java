62d6307df1751c21f7510fc0d2ab5bbba8d2d529
reke592 <erric.rapsing@gmail.com>
2018-03-13 17:31:15 +0800

    initial commit

---
+++ src/main/java/com/reke/hobby/HobbyApplication.java
@@ -0,0 +1,12 @@
+package com.reke.hobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyApplication.class, args);
+	}
+}
